b325b87861cf0f290c2c5c20f7160fee9dec0973
RWEngelbrecht <rigardt@brechtburg.co.za>
2019-10-13 22:20:20 +0200

    login page, redirects to registration page

---
+++ Rush00/customer_login.php
@@ -0,0 +1,24 @@
+<div>
+	<form method="POST" action="">
+		<table width="500" align="center" bgcolor="orange">
+			<tr align="center">
+				<td colspan="3"><h2>Please log in or register</h2></td>
+			</tr>
+			<tr align="center">
+				<td align="right">Email:</td>
+				<td><input type="text" name="email" placeholder="enter email"/></td>
+			</tr>
+			<tr>
+				<td align="right">Password:</td>
+				<td><input type="password" name="pass" placeholder="enter password"/></td>
+			</tr>
+			<tr align="right">
+				<td colspan="3" style="font-size:12px;padding:5px 85px"><a href="checkout.php?forgot_pass">Forgot Password?</a></td>
+			</tr>
+			<tr align="center">
+				<td colspan="3"><input type="submit" name="login" value="Login"/></td>
+			</tr>
+		</table>
+		<h2 style="float:left;padding:5px;text-decoration:none;"><a href="customer_register.php">Register here</a>
+	</form>
+</div>
