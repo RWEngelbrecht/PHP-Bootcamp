f9f49cc3c1153e4be28f4ea15b3f42b4f0a51774
RWEngelbrecht <rengelbr@student.wethinkcode.co.za>
2019-10-14 11:45:11 +0200

    html for login tab

--- Rush00/customer_login.php
+++ Rush00/customer_login.php
@@ -6,11 +6,11 @@
 			</tr>
 			<tr align="center">
 				<td align="right">Email:</td>
-				<td><input type="text" name="email" placeholder="enter email"/></td>
+				<td><input type="text" name="email" placeholder="enter email" required/></td>
 			</tr>
 			<tr>
 				<td align="right">Password:</td>
-				<td><input type="password" name="pass" placeholder="enter password"/></td>
+				<td><input type="password" name="pass" placeholder="enter password" required/></td>
 			</tr>
 			<tr align="right">
 				<td colspan="3" style="font-size:12px;padding:5px 85px"><a href="checkout.php?forgot_pass">Forgot Password?</a></td>
@@ -19,6 +19,25 @@
 				<td colspan="3"><input type="submit" name="login" value="Login"/></td>
 			</tr>
 		</table>
-		<h2 style="float:left;padding:5px;text-decoration:none;"><a href="customer_register.php">Register here</a>
+		<h2 style="float:left; padding-right:300px;color:white;">Don't have an acoount? <a href="customer_register.php" style="text-decoration:none;color:orange;">Register here</a>, it's free! <small style="font-size:10px">mostly...</small></h2>
 	</form>
+	<?php
+		// global $con;
+		// if (isset($_POST['login'])) {
+
+		// 	$cust_email = $_POST['email'];
+		// 	$get_customer = "SELECT * FROM customers WHERE customer_email='$cust_email'";
+		// 	$run_customer = mysqli_query($con, $get_customer);
+		// 	print_r($run_cust);
+		// 	if (mysqli_num_row($run_customer)==0)
+		// 		echo "<script>window.open('customer_register.php','_self')</script>";
+		// 	else {
+		// 		$cust_info = mysqli_fetch_array($run_customer);
+		// 		if ($cust_info['customer_pass'] == $_POST['pass'])
+		// 			echo "<script>window.open('checkout.php','_self')</script>";
+		// 		else
+		// 			echo "<h2 style='color:white'>Password incorrect!</h2>";
+		// 	}
+		// }
+	?>
 </div>
