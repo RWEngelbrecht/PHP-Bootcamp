7212b43885c9ea0c38777de1170d187ca1d86f11
RWEngelbrecht <rengelbr@student.wethinkcode.co.za>
2019-10-16 17:42:01 +0200

    A class fo instantiate vectors with

--- Day06/ex02/Vector.class.php
+++ Day06/ex02/Vector.class.php
@@ -9,17 +9,17 @@ class Vector
 
 	static $verbose = false;
 
-	public function __construct($vector) {
-		if (isset($vector['orig']) && $vector['orig'] instanceof Vertex) {
-			$orig = new Vertex(array('x'=>$vector['orig']->get_x(),
-				'y'=>$vector['orig']->get_y(), 'z'=>$vector['orig']->get_z()));
+	public function __construct($vertex) {
+		if (isset($vertex['orig']) && $vertex['orig'] instanceof Vertex) {
+			$orig = new Vertex(array('x'=>$vertex['orig']->get_x(),
+				'y'=>$vertex['orig']->get_y(), 'z'=>$vertex['orig']->get_z()));
 		}
 		else {
 			$orig = new Vertex(array('x'=>0, 'y'=>0, 'z'=>0));
 		}
-		$this->_x = $vector['dest']->get_x() - $orig->get_x();
-		$this->_y = $vector['dest']->get_y() - $orig->get_y();
-		$this->_z = $vector['dest']->get_z() - $orig->get_z();
+		$this->_x = $vertex['dest']->get_x() - $orig->get_x();
+		$this->_y = $vertex['dest']->get_y() - $orig->get_y();
+		$this->_z = $vertex['dest']->get_z() - $orig->get_z();
 		$this->_w = 0.0;
 		if (Self::$verbose == true) {
 			printf("Vector( x:%0.2f, y:%0.2f, z:%0.2f, w:%0.2f ) constructed\n", $this->_x, $this->_y, $this->_z, $this->_w);
@@ -44,7 +44,77 @@ class Vector
 		return (float)sqrt(($this->_x**2 + $this->_y**2 + $this->_z**2));
 	}
 
+	public function normalize() {
+		$mag = $this->magnitude();
+		if ($mag == 1) {
+			return clone $this;
+		}
+		$normX = $this->_x / $mag;
+		$normY = $this->_y / $mag;
+		$normZ = $this->_z / $mag;
+		$normVer = new Vertex(array('x'=>$normX, 'y'=>$normY, 'z'=>$normZ));
+		$normVec = new Vector(array('dest'=>$normVer));
+		return $normVec;
+	}
 
+	public function add($rhs) {
+		$sumX = $this->_x + $rhs->__get('_x');
+		$sumY = $this->_y + $rhs->__get('_y');
+		$sumZ = $this->_z + $rhs->__get('_z');
+		$sumVer = new Vertex(array('x'=>$sumX, 'y'=>$sumY, 'z'=>$sumZ));
+		$sumVec = new Vector(array('dest'=>$sumVer));
+		return $sumVec;
+	}
+
+	public function sub($rhs) {
+		$subX = $this->_x - $rhs->__get('_x');
+		$subY = $this->_y - $rhs->__get('_y');
+		$subZ = $this->_z - $rhs->__get('_z');
+		$subVer = new Vertex(array('x'=>$subX, 'y'=>$subY, 'z'=>$subZ));
+		$subVec = new Vector(array('dest'=>$subVer));
+		return $subVec;
+	}
+
+	public function opposite() {
+		$oppX = $this->_x * -1;
+		$oppY = $this->_y * -1;
+		$oppZ = $this->_z * -1;
+		$oppVer = new Vertex(array('x'=>$oppX, 'y'=>$oppY, 'z'=>$oppZ));
+		$oppVec = new Vector(array('dest'=>$oppVer));
+		return $oppVec;
+	}
+
+	public function scalarProduct($k) {
+		$scProdX = $this->_x * $k;
+		$scProdY = $this->_y * $k;
+		$scProdZ = $this->_z * $k;
+		$scProdVer = new Vertex(array('x'=>$scProdX, 'y'=>$scProdY, 'z'=>$scProdZ));
+		$scProdVec = new Vector(array('dest'=>$scProdVer));
+		return $scProdVec;
+	}
+
+	public function dotProduct($rhs) {
+		$dProdX = $this->_x*$rhs->__get('_x');
+		$dProdY = $this->_y*$rhs->__get('_y');
+		$dProdZ = $this->_z*$rhs->__get('_z');
+		return (float)($dProdX + $dProdY + $dProdZ);
+	}
+
+	public function cos($rhs) {
+		$dotProd = $this->dotProduct($rhs);
+		$mag1 = $this->magnitude();
+		$mag2 = (float)sqrt($rhs->__get('_x')**2 + $rhs->__get('_y')**2 + $rhs->__get('_z')**2);
+		return $dotProd / ($mag1 * $mag2);
+	}
+
+	public function crossProduct($rhs) {
+		$crosProdX = $this->_y*$rhs->__get('_z') - $this->_z*$rhs->__get('_y');
+		$crosProdY = $this->_z*$rhs->__get('_x') - $this->_x*$rhs->__get('_z');
+		$crosProdZ = $this->_x*$rhs->__get('_y') - $this->_y*$rhs->__get('_x');
+		$crosVer = new Vertex(array('x'=>$crosProdX, 'y'=>$crosProdY, 'z'=>$crosProdZ));
+		$crosVec = new Vector(array('dest'=>$crosVer));
+		return $crosVec;
+	}
 
 	static function doc() {
 		$file = fopen("Vector.doc.txt", 'r');
@@ -54,6 +124,5 @@ class Vector
 		}
 		echo "\n";
 	}
-
 }
 ?>
