05600b5f4ad15cde80d8c443892cbcb06269950b
rengelbr <rengelbr@student.wethinkcode.co.za>
2019-10-19 16:24:42 +0200

    started ex03 - authorising logins

--- Day04/ex02/modif.php
+++ Day04/ex02/modif.php
@@ -2,40 +2,17 @@
 if ($_POST['submit'] != "OK")
 	exit("ERROR\n");
 $user = array('login'=>$_POST['login'], 'passwd'=>hash('whirlpool', $_POST['oldpw']));
-print_r($_POST);
-print_r($user);
 $folder = "../private/";
 $file = "passwd";
 $f_cont = array();
 $f_cont = file_get_contents($folder.$file);
 $f_cont = unserialize($f_cont);
-// print_r($f_cont);
-foreach($f_cont as $usr) {
-	foreach($usr as $key=>$elem) {
-		if ($key == "login") {
-			if ($elem == $user['login']) {
-				$usr_verif = $elem;
-			}
-		}
-		if ($key == "passwd") {
-			if ($elem == $user['passwd'])
-				$newpas = hash('whirlpool', $_POST['newpw']);
-		}
+foreach ($f_cont as $key => $val) {
+	if ($val['login'] == $_POST['login'] && $val['passwd'] == hash('whirlpool', $_POST['oldpw'])) {
+		$f_cont[$key]['passwd'] = hash('whirlpool', $_POST['newpw']);
+		file_put_contents($folder.$file, serialize($f_cont));
+		exit("OK\n");
 	}
 }
-echo "oldpas = ".$user['passwd']."\n"."newpas = $newpas\n";
-if ($usr_verif == null || $newpas == null)
-	exit("ERROR\n");
-foreach($f_cont as $usr) {
-	foreach ($usr as $key=>$elem) {
-		if ($key == "passwd" && $elem == $user['passwd']) {
-			echo "doing this\nusr ==  ";
-			$usr['passwd'] = $newpas;
-			print_r($usr);
-		}
-	}
-}
-print_r($f_cont);
-file_put_contents($folder.$file, serialize($f_cont));
-exit("OK\n")
+exit("ERROR\n");
 ?>
